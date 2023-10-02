8df0dcb110892837e1f556aa714e84c4b9a43152
Toren Arginteanu <targinteanu@outlook.com>
2023-10-02 01:16:54 -0400

    added spectrogram, but results don't make sense, time axis does not make sense, and takes a long time, needs updates

--- plotBeforeAfterStim.m
+++ plotBeforeAfterStim.m
@@ -30,8 +30,8 @@ d_PrePost           = cell(1, length(uchan));
 e_t_PrePost         = cell(size(d_PrePost));
 %e_t_lpf_PrePost     = cell(size(d_PrePost));
 
-for idx = 1:length(uchan)
-    gch = g(:,idx);
+for chIdx = 1:length(uchan)
+    gch = g(:,chIdx);
     trig = [0; abs(diff(gch))];
     trig = trig > .1*max(trig); trig = find(trig);
 
@@ -51,14 +51,14 @@ for idx = 1:length(uchan)
         % time vector
         % consider aligning so that rows of e correspond to d exactly 
         if (tr -nBeforeTrig > 0) & (tr +nBeforeTrig <= size(d,1))
-            d_PrePost_ch(trIdx,:,1) = d(tr + ((-nBeforeTrig):0), idx);
-            d_PrePost_ch(trIdx,:,2) = d(tr + (  0:nBeforeTrig ), idx);
+            d_PrePost_ch(trIdx,:,1) = d(tr + ((-nBeforeTrig):0), chIdx);
+            d_PrePost_ch(trIdx,:,2) = d(tr + (  0:nBeforeTrig ), chIdx);
 %            d_lpf_PrePost_ch(trIdx,:,1) = d_lpf(tr + ((-nBeforeTrig):0), idx);
 %            d_lpf_PrePost_ch(trIdx,:,2) = d_lpf(tr + (  0:nBeforeTrig ), idx);
         end
         if (tr -N+1 -nBeforeTrig > 0) & (tr -N+1 +nBeforeTrig <= size(e_t,1))
-            e_t_PrePost_ch(trIdx,:,1) = e_t(tr -N+1 + ((-nBeforeTrig):0), idx);
-            e_t_PrePost_ch(trIdx,:,2) = e_t(tr -N+1 + (  0:nBeforeTrig ), idx);
+            e_t_PrePost_ch(trIdx,:,1) = e_t(tr -N+1 + ((-nBeforeTrig):0), chIdx);
+            e_t_PrePost_ch(trIdx,:,2) = e_t(tr -N+1 + (  0:nBeforeTrig ), chIdx);
 %            e_t_lpf_PrePost_ch(trIdx,:,1) = e_t_lpf(tr -N+1 + ((-nBeforeTrig):0), idx);
 %            e_t_lpf_PrePost_ch(trIdx,:,2) = e_t_lpf(tr -N+1 + (  0:nBeforeTrig ), idx);
         end
@@ -75,13 +75,13 @@ for idx = 1:length(uchan)
     d_PrePost_ch = d_PrePost_ch(~toRemove,:,:);
 %    d_lpf_PrePost_ch = d_lpf_PrePost_ch(~toRemove,:,:);
 
-    d_PrePost{idx} = d_PrePost_ch;
+    d_PrePost{chIdx} = d_PrePost_ch;
 %    d_lpf_PrePost{idx} = d_lpf_PrePost_ch;
 %    e_train_PrePost{idx} = e_train_PrePost_ch;
 %    e_train_lpf_PrePost{idx} = e_train_lpf_PrePost_ch;
 %    e_test_PrePost{idx} = e_test_PrePost_ch;
 %    e_test_lpf_PrePost{idx} = e_test_lpf_PrePost_ch;
-    e_t_PrePost{idx} = e_t_PrePost_ch;
+    e_t_PrePost{chIdx} = e_t_PrePost_ch;
 %    e_t_lpf_PrePost{idx} = e_t_lpf_PrePost_ch;
 end
 
@@ -90,18 +90,28 @@ clear d_PrePost_ch d_lpf_PrePost_che_train_PrePost_ch e_train_lpf_PrePost_ch
 clear e_test_PrePost_ch e_test_lpf_PrePost_ch e_t_PrePost_ch e_t_lpf_PrePost_ch 
 clear gch trig trIdx tr toRemove
 
-%% plotting averaged signals before and after stim 
-% colors: 
+%% colors: 
 dkBlue  = [  1,  50, 130] /255;
 ltBlue  = [145, 190, 255] /255;
 dkRed   = [110,   0,   0] /255;
 ltRed   = [250, 150, 150] /255;
 dkBlack = [  0,   0,   0] /255;
 ltBlack = [110, 110, 110] /255;
+dkPink  = [130,   1, 125] /255;
+dkAqua  = [  0, 100, 120] /255;
+dkGreen = [  0,  85,  15] /255;
 
-for idx = 1:length(uchan)
-    sigFiltCh = e_t_PrePost{idx};
-    sigUnfiltCh = d_PrePost{idx};
+%% plotting averaged signals before and after stim 
+
+sgramWinLen = .005; % s
+sgramWinLen = ceil(max(32, sgramWinLen/Fs)); % samples 
+sgramFmax = Fs/2.25; 
+sgramFWinLen = 4/(tBeforeTrig); % Hz
+sgramFQ = 0:sgramFWinLen:sgramFmax; 
+
+for chIdx = 1:length(uchan)
+    sigFiltCh = e_t_PrePost{chIdx};
+    sigUnfiltCh = d_PrePost{chIdx};
 
     meanFiltBefore = mean(sigFiltCh(:,:,1));
     meanFiltAfter  = mean(sigFiltCh(:,:,2));
@@ -111,6 +121,27 @@ for idx = 1:length(uchan)
     meanUnfiltAfter  = mean(sigUnfiltCh(:,:,2));
     errbUnfiltBefore =  std(sigUnfiltCh(:,:,1));
     errbUnfiltAfter  =  std(sigUnfiltCh(:,:,2));
+
+    [sFiltBefore, sFiltBeforeF, sFiltBeforeT] = ...
+        spectrogram(meanFiltBefore, sgramWinLen, [], sgramFQ);
+    [sFiltAfter, sFiltAfterF, sFiltAfterT] = ...
+        spectrogram(meanFiltAfter, sgramWinLen, [], sgramFQ);
+    [sUnfiltBefore, sUnfiltBeforeF, sUnfiltBeforeT] = ...
+        spectrogram(meanUnfiltBefore, sgramWinLen, [], sgramFQ);
+    [sUnfiltAfter, sUnfiltAfterF, sUnfiltAfterT] = ...
+        spectrogram(meanUnfiltAfter, sgramWinLen, [], sgramFQ);
+    sFiltBefore = zeros([size(sFiltBefore),size(sigFiltCh,1)]);
+    sFiltAfter = zeros([size(sFiltAfter),size(sigFiltCh,1)]);
+    sUnfiltBefore = zeros([size(sUnfiltBefore),size(sigUnfiltCh,1)]);
+    sUnfiltAfter = zeros([size(sUnfiltAfter),size(sigUnfiltCh,1)]);
+    for trl = 1:size(sigFiltCh,1)
+        sFiltBefore(:,:,trl) = spectrogram(sigFiltCh(trl,:,1), sgramWinLen, [], sgramFQ);
+        sFiltAfter(:,:,trl) = spectrogram(sigFiltCh(trl,:,2), sgramWinLen, [], sgramFQ);
+    end
+    for trl = 1:size(sigUnfiltCh,1)
+        sUnfiltBefore(:,:,trl) = spectrogram(sigUnfiltCh(trl,:,1), sgramWinLen, [], sgramFQ);
+        sUnfiltAfter(:,:,trl) = spectrogram(sigUnfiltCh(trl,:,2), sgramWinLen, [], sgramFQ);
+    end
 
     [wFiltBefore, spectFiltBeforeCh] = PowerSpectrum(sigFiltCh(:,:,1), Fs);
     [wFiltAfter, spectFiltAfterCh] = PowerSpectrum(sigFiltCh(:,:,2), Fs);
@@ -133,10 +164,10 @@ for idx = 1:length(uchan)
     meanSpectUnfiltAfter = mean(spectUnfiltAfterCh); 
     errbSpectUnfiltAfter =  std(spectUnfiltAfterCh);
 
-    fig(idx) = figure('Units','normalized', 'Position',[.1 .1 .8 .8]); 
-    figure(fig(idx)); sgtitle(['Channel ',num2str(uchan(idx)),' Avg. Response to Stim']);
+    fig(chIdx,1) = figure('Units','normalized', 'Position',[.1 .1 .8 .8]); 
+    figure(fig(chIdx,1)); sgtitle(['Channel ',num2str(uchan(chIdx)),' Avg. Response to Stim']);
 
-    figure(fig(idx)); subplot(321); 
+    figure(fig(chIdx,1)); subplot(5,2,1); 
            plotWithDistrib(t_PrePost(1,:), meanUnfiltBefore, errbUnfiltBefore, ltRed);
     yrng = plotWithDistrib(t_PrePost(1,:), meanFiltBefore, errbFiltBefore, dkBlue);
     title('Filtered Before'); grid on; 
@@ -144,7 +175,7 @@ for idx = 1:length(uchan)
     ylim(yrng(2,:)); 
     legend('Unfiltered', '-1SD', '+1SD', 'Filtered', '-1SD', '+1SD', 'Location','eastoutside');
 
-    figure(fig(idx)); subplot(322); 
+    figure(fig(chIdx,1)); subplot(5,2,2); 
            plotWithDistrib(t_PrePost(2,:), meanUnfiltAfter, errbUnfiltAfter, ltRed);
     yrng = plotWithDistrib(t_PrePost(2,:), meanFiltAfter, errbFiltAfter, dkBlue);
     title('Filtered After'); grid on; 
@@ -152,7 +183,7 @@ for idx = 1:length(uchan)
     ylim(yrng(2,:));
     legend('Unfiltered', '-1SD', '+1SD', 'Filtered', '-1SD', '+1SD', 'Location','eastoutside');
 
-    figure(fig(idx)); subplot(323);  
+    figure(fig(chIdx,1)); subplot(5,2,3);  
            plotWithDistrib(t_PrePost(1,:), meanFiltBefore, errbFiltBefore, ltBlue);
     yrng = plotWithDistrib(t_PrePost(1,:), meanUnfiltBefore, errbUnfiltBefore, dkRed);
     title('Unfiltered Before'); grid on; 
@@ -160,7 +191,7 @@ for idx = 1:length(uchan)
     ylim(yrng(2,:));
     legend('Filtered', '-1SD', '+1SD', 'Unfiltered', '-1SD', '+1SD', 'Location','eastoutside');
 
-    figure(fig(idx)); subplot(324);  
+    figure(fig(chIdx,1)); subplot(5,2,4);  
            plotWithDistrib(t_PrePost(2,:), meanFiltAfter, errbFiltAfter, ltBlue);
     yrng = plotWithDistrib(t_PrePost(2,:), meanUnfiltAfter, errbUnfiltAfter, dkRed);
     title('Unfiltered After'); grid on; 
@@ -168,7 +199,7 @@ for idx = 1:length(uchan)
     ylim(yrng(2,:));
     legend('Filtered', '-1SD', '+1SD', 'Unfiltered', '-1SD', '+1SD', 'Location','eastoutside');
 
-    figure(fig(idx)); subplot(325); 
+    figure(fig(chIdx,1)); subplot(5,2,5); 
     semilogy(wUnfiltAfter, meanSpectUnfiltAfter, 'Color', ltRed); hold on; 
     semilogy(wFiltAfter, meanSpectFiltAfter, 'Color', ltBlue);
     plotWithDistrib(wUnfiltBefore, meanSpectUnfiltBefore, errbSpectUnfiltBefore, dkRed);
@@ -179,7 +210,7 @@ for idx = 1:length(uchan)
     legend('Unfiltered After', 'Filtered After', ...
         'Unfiltered Before', '-1SD', '+1SD', 'Filtered Before', '-1SD', '+1SD', 'Location','eastoutside');
 
-    figure(fig(idx)); subplot(326); 
+    figure(fig(chIdx,1)); subplot(5,2,6); 
     semilogy(wUnfiltBefore, meanSpectUnfiltBefore, 'Color', ltRed); hold on; 
     semilogy(wFiltBefore, meanSpectFiltBefore, 'Color', ltBlue);
     plotWithDistrib(wUnfiltAfter, meanSpectUnfiltAfter, errbSpectUnfiltAfter, dkRed);
@@ -189,7 +220,29 @@ for idx = 1:length(uchan)
     xlabel('Frequency (Hz)'); ylabel('Power Spectrum (V^2*s^2)');
     legend('Unfiltered Before', 'Filtered Before', ...
         'Unfiltered After', '-1SD', '+1SD', 'Filtered After', '-1SD', '+1SD', 'Location','eastoutside');
+
+    figure(fig(chIdx,1)); subplot(5,2,7);
+    imagesc(sFiltBeforeT, sFiltBeforeF, abs(mean(sFiltBefore,3))); colorbar;
+    title('Spectrogram Filtered Before (dB/Hz)'); 
+    xlabel('time (s)'); ylabel('Frequency (Hz)');
+
+    figure(fig(chIdx,1)); subplot(5,2,8);
+    imagesc(sFiltAfterT, sFiltAfterF, log10(mean(abs(sFiltAfter),3))); colorbar;
+    title('Spectrogram Filtered After (dB/Hz)'); 
+    xlabel('time (s)'); ylabel('Frequency (Hz)');
+
+    figure(fig(chIdx,1)); subplot(5,2,9);
+    imagesc(sUnfiltBeforeT, sUnfiltBeforeF, abs(mean(sUnfiltBefore,3))); colorbar;
+    title('Spectrogram Unfiltered Before (dB/Hz)'); 
+    xlabel('time (s)'); ylabel('Frequency (Hz)');
+
+    figure(fig(chIdx,1)); subplot(5,2,10);
+    imagesc(sUnfiltAfterT, sUnfiltAfterF, abs(mean(sUnfiltAfter,3))); colorbar;
+    title('Spectrogram Unfiltered After (dB/Hz)'); 
+    xlabel('time (s)'); ylabel('Frequency (Hz)');
 end
+
+%% Plotting averaged - in batches 
 
 %% helper functions 
 function range = plotWithDistrib(x, y, dist, colr)
