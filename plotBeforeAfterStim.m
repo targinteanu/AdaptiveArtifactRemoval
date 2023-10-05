cc4091a323a904904c7360462a15537d231e2cd4
Toren <toren@thakors-imac.acasmart.jh.edu>
2023-10-05 16:05:01 -0400

    cleaned out comments and changed variable names

--- plotBeforeAfterStim.m
+++ plotBeforeAfterStim.m
@@ -1,4 +1,4 @@
-function fig = plotBeforeAfterStim(tBeforeTrig, g, d, e_t, Fs, uchan, N, nTrlInBatch, nUpdates)
+function fig = plotBeforeAfterStim(tBeforeTrig, g, d, e, Fs, uchan, N, nTrlInBatch, nUpdates)
 % Plot the filtered and unfiltered signals and their power spectra some
 % time before and after the stimulus. 
 % 
@@ -6,9 +6,9 @@ function fig = plotBeforeAfterStim(tBeforeTrig, g, d, e_t, Fs, uchan, N, nTrlInB
 %   tBeforeTrig: time before/after stimulus to plot (s) 
 %   g: stimulus value over time, as columns 
 %   d: unfiltered value over time, as columns (V)
-%   e_t: filtered value over time, as columns (V)
+%   e: filtered value over time, as columns (V)
 %   Fs: sampling rate of g, d, and e_t (Hz) 
-%   uchan: array of unique channels, same length as width of g, d, and e_t 
+%   uchan: array of unique channels, same length as width of g, d, and e 
 %   N: number of filter taps         
 % 
 % Outputs: 
@@ -22,13 +22,7 @@ tBeforeTrig = nBeforeTrig/Fs;
 t_PrePost = [-tBeforeTrig:(1/Fs):0; 0:(1/Fs):tBeforeTrig]; % [before; after]
 
 d_PrePost           = cell(1, length(uchan));
-%d_lpf_PrePost       = cell(size(d_PrePost));
-%e_train_PrePost     = cell(size(d_PrePost));
-%e_train_lpf_PrePost = cell(size(d_PrePost));
-%e_test_PrePost      = cell(size(d_PrePost));
-%e_test_lpf_PrePost  = cell(size(d_PrePost));
-e_t_PrePost         = cell(size(d_PrePost));
-%e_t_lpf_PrePost     = cell(size(d_PrePost));
+e_PrePost         = cell(size(d_PrePost));
 
 for chIdx = 1:length(uchan)
     gch = g(:,chIdx);
@@ -36,13 +30,7 @@ for chIdx = 1:length(uchan)
     trig = trig > .1*max(trig); trig = find(trig);
 
     d_PrePost_ch           = nan(length(trig), nBeforeTrig+1, 2);
-%    d_lpf_PrePost_ch       = nan(size(d_PrePost_ch));
-%    e_train_PrePost_ch     = nan(size(d_PrePost_ch));
-%    e_train_lpf_PrePost_ch = nan(size(d_PrePost_ch));
-%    e_test_PrePost_ch      = nan(size(d_PrePost_ch));
-%    e_test_lpf_PrePost_ch  = nan(size(d_PrePost_ch));
-    e_t_PrePost_ch         = nan(size(d_PrePost_ch));
-%    e_t_lpf_PrePost_ch     = nan(size(d_PrePost_ch));
+    e_PrePost_ch           = nan(size(d_PrePost_ch));
 
     for trIdx = 1:length(trig)
         tr = trig(trIdx); % timepoint
@@ -53,41 +41,29 @@ for chIdx = 1:length(uchan)
         if (tr -nBeforeTrig > 0) & (tr +nBeforeTrig <= size(d,1))
             d_PrePost_ch(trIdx,:,1) = d(tr + ((-nBeforeTrig):0), chIdx);
             d_PrePost_ch(trIdx,:,2) = d(tr + (  0:nBeforeTrig ), chIdx);
-%            d_lpf_PrePost_ch(trIdx,:,1) = d_lpf(tr + ((-nBeforeTrig):0), idx);
-%            d_lpf_PrePost_ch(trIdx,:,2) = d_lpf(tr + (  0:nBeforeTrig ), idx);
         end
-        if (tr -N+1 -nBeforeTrig > 0) & (tr -N+1 +nBeforeTrig <= size(e_t,1))
-            e_t_PrePost_ch(trIdx,:,1) = e_t(tr -N+1 + ((-nBeforeTrig):0), chIdx);
-            e_t_PrePost_ch(trIdx,:,2) = e_t(tr -N+1 + (  0:nBeforeTrig ), chIdx);
-%            e_t_lpf_PrePost_ch(trIdx,:,1) = e_t_lpf(tr -N+1 + ((-nBeforeTrig):0), idx);
-%            e_t_lpf_PrePost_ch(trIdx,:,2) = e_t_lpf(tr -N+1 + (  0:nBeforeTrig ), idx);
+        if (tr -N+1 -nBeforeTrig > 0) & (tr -N+1 +nBeforeTrig <= size(e,1))
+            e_PrePost_ch(trIdx,:,1) = e(tr -N+1 + ((-nBeforeTrig):0), chIdx);
+            e_PrePost_ch(trIdx,:,2) = e(tr -N+1 + (  0:nBeforeTrig ), chIdx);
         end
     end
 
-    toRemove = sum(e_t_PrePost_ch,2); 
+    toRemove = sum(e_PrePost_ch,2); 
     toRemove = sum(toRemove,3);
     toRemove = isnan(toRemove);
-    e_t_PrePost_ch = e_t_PrePost_ch(~toRemove,:,:);
-%    e_t_lpf_PrePost_ch = e_t_lpf_PrePost_ch(~toRemove,:,:);
+    e_PrePost_ch = e_PrePost_ch(~toRemove,:,:);
     toRemove = sum(d_PrePost_ch,2); 
     toRemove = sum(toRemove,3);
     toRemove = isnan(toRemove);
     d_PrePost_ch = d_PrePost_ch(~toRemove,:,:);
-%    d_lpf_PrePost_ch = d_lpf_PrePost_ch(~toRemove,:,:);
 
     d_PrePost{chIdx} = d_PrePost_ch;
-%    d_lpf_PrePost{idx} = d_lpf_PrePost_ch;
-%    e_train_PrePost{idx} = e_train_PrePost_ch;
-%    e_train_lpf_PrePost{idx} = e_train_lpf_PrePost_ch;
-%    e_test_PrePost{idx} = e_test_PrePost_ch;
-%    e_test_lpf_PrePost{idx} = e_test_lpf_PrePost_ch;
-    e_t_PrePost{chIdx} = e_t_PrePost_ch;
-%    e_t_lpf_PrePost{idx} = e_t_lpf_PrePost_ch;
+    e_PrePost{chIdx} = e_PrePost_ch;
 end
 
 %% cleanup 
 clear d_PrePost_ch d_lpf_PrePost_che_train_PrePost_ch e_train_lpf_PrePost_ch 
-clear e_test_PrePost_ch e_test_lpf_PrePost_ch e_t_PrePost_ch e_t_lpf_PrePost_ch 
+clear e_test_PrePost_ch e_test_lpf_PrePost_ch e_PrePost_ch e_t_lpf_PrePost_ch 
 clear gch trig trIdx tr toRemove
 
 %% colors: 
@@ -111,7 +87,7 @@ sgramFWinLen = 4/(tBeforeTrig); % Hz
 sgramFQ = 0:sgramFWinLen:sgramFmax; 
 
 for chIdx = 1:length(uchan)
-    sigFiltCh = e_t_PrePost{chIdx};
+    sigFiltCh = e_PrePost{chIdx};
     sigUnfiltCh = d_PrePost{chIdx};
 
     meanFiltBefore = mean(sigFiltCh(:,:,1));
@@ -264,7 +240,7 @@ clear sgramFQ sgramWinLen sgramFmax sgramFWinLen
 
 %% Plotting averaged - in batches 
 for chIdx = 1:length(uchan)
-    sigFiltCh = e_t_PrePost{chIdx};
+    sigFiltCh = e_PrePost{chIdx};
     sigUnfiltCh = d_PrePost{chIdx};
 
     [wFiltBefore, spectFiltBeforeCh] = PowerSpectrum(sigFiltCh(:,:,1), Fs);
@@ -447,7 +423,7 @@ clear curBatchTrlIdx batchIdx curBatch theta batchColor
 
 %% Plotting Stats
 for chIdx = 1:length(uchan)
-    sigFiltCh = e_t_PrePost{chIdx};
+    sigFiltCh = e_PrePost{chIdx};
     sigUnfiltCh = d_PrePost{chIdx};
 
     sigFiltBeforeCh = sigFiltCh(:,:,1);
