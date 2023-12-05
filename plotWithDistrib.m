a8a1d52b98617bc7990bb535aa1b975de5fe707b
Toren Arginteanu <torenarginteanu@Torens-MacBook-Pro.local>
2023-12-04 20:56:08 -0600

    line width

--- plotWithDistrib.m
+++ plotWithDistrib.m
@@ -1,10 +1,10 @@
 function range = plotWithDistrib(x, y, dist, colr)
     % plot y with a dashed +- distribution surrounding y. 
     % y and dist must be row vectors
-    plot(x, y, 'Color', colr); 
+    plot(x, y, 'Color', colr, 'LineWidth',1); 
     hold on; 
     Y = y + [1;-1].*dist;
-    plot(x, Y, ':', 'Color', colr);
+    plot(x, Y, ':', 'Color', colr, 'LineWidth',1);
     range = [min(Y(:)), max(Y(:))];
     range = [range; 1.25*[-1,1]*.5*diff(range) + mean(range)];
 end
