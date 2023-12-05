43cd02495b4ebe973652ac102f6d88461d036568
Toren Arginteanu <torenarginteanu@torens-mbp.acasmart.jh.edu>
2023-12-05 15:02:41 -0500

    changed HPF to filtfilt to match LPF

--- RodentSSEPDataFilt.m
+++ RodentSSEPDataFilt.m
@@ -131,7 +131,7 @@ hpFilt = designfilt('highpassiir', ...
                     'StopbandAttenuation', 60, ...
                     'SampleRate', Fs, ... 
                     'DesignMethod', 'butter');
-d         = filter(hpFilt, d_unfilt);
+d         = filtfilt(hpFilt, d_unfilt);
 %% lowpass filtering (noise removal)
 lpFilt = designfilt('lowpassiir', ...
                     'StopbandFrequency', 2000, ...
@@ -142,11 +142,20 @@ lpFilt = designfilt('lowpassiir', ...
                     'DesignMethod', 'cheby2');
 %fvtool(lpFilt);
 
+%% filter to object 
+filtObj = buildFilterObj(hpFilt, lpFilt, N, stepsize);
+
 %% pretraining and testing 
+%%{
 [t_train, g_train, d_train, t_test, g_test, d_test] = ...
     getTrainTest(t, g, d, tTrainBnd, uchan);
 [w, e_train, op_train, fig] = preTrainWts(t_train, g_train, d_train, N, uchan, .1*nUpdates);
+%}
 %[e_test, op_test] = testPreTrained(w, t_test, g_test, d_test, N, uchan, .1*nUpdates);
+%{
+sig = getTrainTestWrapper(sig);
+[sig, filtObj] = preTrainWtsWrapper(filtObj, sig, .1*nUpdates);
+%}
 
 %% online LMS 
 [e_t, w_OL] = LMSonline(t, g, d, stepsize, N, uchan, w, nUpdates);
