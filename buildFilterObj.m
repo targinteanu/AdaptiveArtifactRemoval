4127fb96f2b1ccba085f4547b4266ad0d8e8c02f
Toren Arginteanu <torenarginteanu@torens-mbp.acasmart.jh.edu>
2023-12-05 15:02:52 -0500

    fixed error

--- buildFilterObj.m
+++ buildFilterObj.m
@@ -1,4 +1,12 @@
-function filtObj = buildFilterObj(HPF, LPF, Num_Taps, Step_Size)
+function filtObj = buildFilterObj(HPF, LPF, Num_Taps, Step_Size, ...
+    Starting_Weights, Current_Weights)
+
+if nargin < 5
+    Starting_Weights = [];
+end
+if nargin < 6
+    Current_Weights = Starting_Weights;
+end
 
 filtObj.HPF = HPF; 
 filtObj.LPF = LPF; 
