9233cd12fcc726e0e870215b54b7c028cd613599
Toren Arginteanu <torenarginteanu@MacBook-Pro.localdomain>
2024-01-08 23:38:32 -0500

    fixed issue

--- measureERP.m
+++ measureERP.m
@@ -12,16 +12,17 @@ pOut = zeros(size(pPk));
 nOut = zeros(size(nPk));
 
 [npk, nlc, nw, npr] = findpeaks( x, t, 'MinPeakProminence', .1*xRng);
-%{
-figure; subplot(211); findpeaks( x, t, 'MinPeakProminence', .1*xRng, 'Annotate', 'extents');
-subplot(212); stem(npr/xRng);
-%}
 [ppk, plc, pw, ppr] = findpeaks(-x, t, 'MinPeakProminence', .1*xRng);
+%{
+figure; subplot(211); findpeaks(-x, t, 'MinPeakProminence', .1*xRng, 'Annotate', 'extents');
+subplot(212); stem(ppr/xRng);
+%}
 
 pPksList = candidatePeaks(pPk, ppk, plc, pw, ppr);
 nPksList = candidatePeaks(nPk, npk, nlc, nw, npr);
 
 pOut = selectPeaks(pPk, pPksList); nOut = selectPeaks(nPk, nPksList);
+pOut(1,:) = -pOut(1,:);
 
     function pksList = candidatePeaks(locDesired, pk, lc, w, pr)
         pksList = cell(size(locDesired));
@@ -38,7 +39,7 @@ pOut = selectPeaks(pPk, pPksList); nOut = selectPeaks(nPk, nPksList);
             desloc = des(i); 
             candi = cand{i};
             tscore = abs(desloc - candi(:,2))./tRng;
-            pscore = abs(candi(:,1))./xRng; 
+            pscore = candi(:,4)./xRng; 
             score = pscore .* exp(-7*tscore);
             [~,sel] = max(score);
             lcOut(1,i) = candi(sel,1);
