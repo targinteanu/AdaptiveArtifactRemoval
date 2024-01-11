86d300d90ac0ac2d2a4bf30d7108511956096c94
Toren <toren@thakors-imac.acasmart.jh.edu>
2024-01-11 17:48:28 -0500

    started evaluating step sizes via trial and error

--- RodentSensoryNerveStim.m
+++ RodentSensoryNerveStim.m
@@ -28,11 +28,41 @@ chB = buildChannelObj('insert_name_here', 0,0,0, 'Cartesian');
 % (t and g should have the same number of rows as d_unfilt see repmat)
 %% define parameters for filter and training 
 trainfrac = .1;
-N = 64; % filter taps 
-stepsize = .2;
 nUpdates = 100;
 splIdx = floor(trainfrac*size(t,1));
 tTrainBnd = [t(1), t(splIdx)];
 
 sig = buildSignalObj([], d_unfilt, t, g, Fs, [chA; chB], ...
                      tTrainBnd, tTrainBnd, 2);
+
+%% filtering
+% highpass filtering (baseline removal) 
+hpFilt = designfilt('highpassiir', ...
+                    'StopbandFrequency', .5, ...
+                    'PassbandFrequency', 1.5, ...
+                    'PassbandRipple', .5, ...
+                    'StopbandAttenuation', 60, ...
+                    'SampleRate', Fs, ... 
+                    'DesignMethod', 'butter');
+% lowpass filtering (noise removal)
+lpFilt = designfilt('lowpassiir', ...
+                    'StopbandFrequency', 2000, ...
+                    'PassbandFrequency', 1500, ...
+                    'PassbandRipple', .5, ...
+                    'StopbandAttenuation', 60, ...
+                    'SampleRate', Fs, ... 
+                    'DesignMethod', 'cheby2');
+N = 128; % filter taps 
+stepsize = .1;
+filtObj = buildFilterObj(hpFilt, lpFilt, N, stepsize, true);
+sig = doHPFilt(filtObj, sig);
+[sig, w_end] = LMSonlineWrapper(filtObj, sig, nUpdates);
+
+
+plotwindow = 300000:600000;
+figure; 
+plot(sig.Times(plotwindow), sig.Data_HPF(plotwindow,2));
+hold on; 
+plot(sig.Times(plotwindow), sig.Data_LMS(plotwindow,2));
+legend('Unfiltered', 'Filtered');
+grid on;
