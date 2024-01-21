468b6db92f45b5aa92f0744a0c2ba0219b6d2a75
Toren Arginteanu <torenarginteanu@Torens-MacBook-Pro.local>
2024-01-21 16:39:11 -0500

    added filtering steps and plot details

--- RodentSensoryNerveStim.m
+++ RodentSensoryNerveStim.m
@@ -1,8 +1,8 @@
 %% load file
-%load('rat1/amplifier_data/Naive/rat1_N_CMAP_1_230731_113111'); % load the "filt" 2D array 
-load('D:\filtering research proj\rat1\rat1\amplifier_data\Naive\rat1_N_CMAP_1_230731_113111.mat');
-%load('rat1/trigger_data/Naive/rat1_N_CMAP_1_230731_113111_trigger'); % load the "board_adc_data" 1D array 
-load('D:\filtering research proj\rat1\rat1\trigger_data\Naive\rat1_N_CMAP_1_230731_113111_trigger.mat');
+load('rat1/amplifier_data/Naive/rat1_N_CMAP_1_230731_113111'); % load the "filt" 2D array 
+%load('D:\filtering research proj\rat1\rat1\amplifier_data\Naive\rat1_N_CMAP_1_230731_113111.mat');
+load('rat1/trigger_data/Naive/rat1_N_CMAP_1_230731_113111_trigger'); % load the "board_adc_data" 1D array 
+%load('D:\filtering research proj\rat1\rat1\trigger_data\Naive\rat1_N_CMAP_1_230731_113111_trigger.mat');
 
 %% define timing
 Fs = 20000; % samples per second 
@@ -22,8 +22,8 @@ d_unfilt = d_unfilt';
 g = [board_adc_data;board_adc_data];
 g = g';
 
-chA = buildChannelObj('insert_name_here', 0,0,0, 'Cartesian');
-chB = buildChannelObj('insert_name_here', 0,0,0, 'Cartesian');
+chA = buildChannelObj('Chan1', 0,0,0, 'Cartesian');
+chB = buildChannelObj('Chan2', 0,0,0, 'Cartesian');
 
 % (t and g should have the same number of rows as d_unfilt see repmat)
 %% define parameters for filter and training 
@@ -46,23 +46,32 @@ hpFilt = designfilt('highpassiir', ...
                     'DesignMethod', 'butter');
 % lowpass filtering (noise removal)
 lpFilt = designfilt('lowpassiir', ...
-                    'StopbandFrequency', 2000, ...
-                    'PassbandFrequency', 1500, ...
+                    'StopbandFrequency', 1500, ...
+                    'PassbandFrequency', 1000, ...
                     'PassbandRipple', .5, ...
                     'StopbandAttenuation', 60, ...
                     'SampleRate', Fs, ... 
                     'DesignMethod', 'cheby2');
-N = 128; % filter taps 
-stepsize = .1;
+N = 64; % filter taps 
+stepsize = .05;
 filtObj = buildFilterObj(hpFilt, lpFilt, N, stepsize, true);
 sig = doHPFilt(filtObj, sig);
 [sig, w_end] = LMSonlineWrapper(filtObj, sig, nUpdates);
+sig = doLPFilt(filtObj, sig);
 
-
-plotwindow = 300000:600000;
-figure; 
-plot(sig.Times(plotwindow), sig.Data_HPF(plotwindow,2));
+%%
+plotwindow1 = 1:600000;
+plotwindow2 = plotwindow1 + N;
+figure('Units','normalized', 'Position',[.1,.1,.8,.8]); 
+ax(1) = subplot(211);
+plot(sig.Times(plotwindow1), sig.Data_BPF(plotwindow1,2));
 hold on; 
-plot(sig.Times(plotwindow), sig.Data_LMS(plotwindow,2));
+plot(sig.Times(plotwindow2), sig.Data_LMS_LPF(plotwindow1,2));
 legend('Unfiltered', 'Filtered');
+xlabel('time (s)'); ylabel('Volts'); title('signal');
 grid on;
+ax(2) = subplot(212); 
+plot(sig.Times(plotwindow1), sig.Noise_Reference(plotwindow1,2));
+xlabel('time (s)'); ylabel('current (amps?)'); title('noise reference');
+grid on;
+linkaxes(ax,'x');
