4322488201a8ba50ffd11d4b9889906a55ea649f
Toren Arginteanu <torenarginteanu@Torens-MacBook-Pro.local>
2024-01-21 23:36:38 -0500

    added ability to switch between LMS and dLMS

--- LMSonline.m
+++ LMSonline.m
@@ -1,4 +1,4 @@
-function [e_t, w, fig] = LMSonline(t, g, d, stepsize, N, uchan, w, nUpdates)
+function [e_t, w, fig] = LMSonline(t, g, d, stepsize, N, uchan, w, nUpdates, dLMS)
 % Perform online LMS adaptive filtering. 
 % 
 % Inputs: 
@@ -13,18 +13,22 @@ function [e_t, w, fig] = LMSonline(t, g, d, stepsize, N, uchan, w, nUpdates)
 %   nUpdates: how many times to display progress and whether or not to plot weights 
 %             and smoothed error. 0 = no output and no plots 
 %             Default = 100
+%   dLMS: if false, does LMS; if true, does dLMS. Default false
 % 
 % Outputs: 
 %   e_t: LMS error signal 
 %   w: final weights 
 %   fig: matlab figure showing the resulting weights and error signal 
 
-if nargin < 7
-    nUpdates = 100;
-    if nargin < 6
-        w = zeros(N,size(d,2));
-        if nargin < 5
-            uchan = 1:size(d,2);
+if nargin < 9
+    dLMS = false;
+    if nargin < 8
+        nUpdates = 100;
+        if nargin < 7
+            w = zeros(N,size(d,2));
+            if nargin < 6
+                uchan = 1:size(d,2); % need to fix
+            end
         end
     end
 end
@@ -45,7 +49,12 @@ for idx = 1:length(uchan)
         xlabel('tap'); ylabel('weight');
         subplot(length(uchan),2,2*idx);   eplot = semilogy(e_t(:,idx)); grid on;
         title(['Channel ',ch.labels,' online']);
-        xlabel('timepoint'); ylabel('e^2');
+        xlabel('timepoint'); 
+        if ~dLMS
+            ylabel('e^2');
+        else
+            ylabel('(de/dt)^2');
+        end
         pause(.5);
     end
     Gprev = zeros(1,N); Eprev = zeros(1,N);
@@ -53,13 +62,21 @@ for idx = 1:length(uchan)
         Gidx = g((1:N)+ep-1, idx)';
         E = d(ep+N-1,idx) - Gidx*w(:,idx);
         e_t(ep, idx) = E;
-        %dw = E*Gidx'; % LMS
-        dw = (E-Eprev) * (Gidx-Gprev)'; % dLMS
+        if ~dLMS
+            dw = E*Gidx'; % LMS
+        else
+            dw = (E-Eprev) * (Gidx-Gprev)'; % dLMS
+        end
         w(:,idx) = w(:,idx) + stepsize*dw;
         Gprev = Gidx; Eprev = E;
         if nUpdates
             if ~mod(ep, floor(size(t,1)/nUpdates))
-                wplot.YData = w(:,idx); eplot.YData = movmean(e_t(:,idx).^2, 5000);
+                wplot.YData = w(:,idx); 
+                edata = e_t(:,idx);
+                if dLMS
+                    edata = [0,diff(edata)];
+                end
+                eplot.YData = movmean(edata.^2, 5000);
                 disp(['Online Channel ',ch.labels,': ',num2str(100*ep/size(t,1)),'%'])
                 pause(eps);
             end
