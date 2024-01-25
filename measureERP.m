da4b1a25e92f0bf28230178342b9341895b4840b
Toren Arginteanu <torenarginteanu@torens-mbp.acasmart.jh.edu>
2024-01-25 15:26:32 -0500

    included comments and input for toggling debugging plot

--- measureERP.m
+++ measureERP.m
@@ -1,17 +1,42 @@
-function [nOut, pOut] = measureERP(t, x, nPk, pPk, tRng)
+function [nOut, pOut] = measureERP(t, x, nPk, pPk, tRng, showPlot)
+% 
+% Find peak latencies and properties from an ERP evoked potential signal.
+% Input desired/approximate latencies; returns found peaks prioritizing
+% both larger amplitude and closer priximity to desired latency. 
+% 
+% Inputs: 
+%   t: time series 
+%   x: signal corresponding to t
+%   nPk: approximate latencies of upward-convex peaks 
+%   pPk: approximate latencies of downward-convex peaks
+%   tRng: range of possible latencies to search, in same units as t
+%   showPlot: if true, displays plot of identified peaks. (default = false)
+% 
+% Outputs: 
+%   nOut: found nPk as [amplitudes; latencies] 
+%   pOut: found pPk as [amplitudes; latencies]
+
+if nargin < 6
+    % default: if showPlot not specified, do not show 
+    showPlot = false;
+end
 
 if ~isempty(tRng) 
+    % if specified (nonempty), limit x and t to the allowed points 
     tIdx = (t >= tRng(1)) & (t <= tRng(2));
     t = t(tIdx);
     x = x(tIdx);
 end
-tRng = max(t) - min(t);
-xRng = max(x) - min(x);
+tRng = max(t) - min(t); % domain of all times 
+xRng = max(x) - min(x); % range of signal 
 
 pOut = zeros(size(pPk)); 
 nOut = zeros(size(nPk));
 
+% get n peak candidates that exceed promience threshold compared to signal
+% range (excludes very small peaks/oscillations) 
 [npk, nlc, nw, npr] = findpeaks( x, t, 'MinPeakProminence', .1*xRng);
+% flip the signal to get p peak candidates 
 [ppk, plc, pw, ppr] = findpeaks(-x, t, 'MinPeakProminence', .1*xRng);
 
 if ~isempty(pPk)
@@ -25,15 +50,21 @@ nPksList = candidatePeaks(nPk, npk, nlc, nw, npr);
 nOut = selectPeaks(nPk, nPksList);
 end
 
-% plotting: add input to enable 
-%{
+if showPlot
+% plot (for debugging)  
 figure; plot(t, x); grid on; 
 hold on; 
 plot(nOut(2,:),nOut(1,:),'^');
 plot(pOut(2,:),pOut(1,:),'v');
-%}
+xlabel('time'); ylabel('amplitude');
+legend('signal', 'n peak(s)', 'p peak(s)');
+end
 
     function pksList = candidatePeaks(locDesired, pk, lc, w, pr)
+        % match found peaks to the nearest desired latency 
+        % return a cell array corresponding to each desired latency 
+        %   each cell contains a list of found peak candidates as rows;
+        %   columns are [amplitude, latency, width, prominence] 
         pksList = cell(size(locDesired));
         for i = 1:length(pk)
             tdiff = abs(lc(i) - locDesired);
@@ -43,14 +74,21 @@ plot(pOut(2,:),pOut(1,:),'v');
     end
 
     function lcOut = selectPeaks(des, cand)
+        % for each desired latency, pick the "best" candidate peak 
+        % return list of peaks as columns; rows are [amplitude; latency]
         lcOut = zeros(2, length(des));
         for i = 1:length(des)
             desloc = des(i); 
             candi = cand{i};
             if isempty(candi)
+                % return nan amplitide and latency if no suitable candidate
+                % near this desired latency was found. 
                 lcOut(1,i) = nan;
                 lcOut(2,i) = nan;
             else
+                % "best" candidate defined by score preferring larger value
+                % normalized to amplitude range (pscore) and exponentially 
+                % penalizing latency distance normalized to time range (tscore)
                 tscore = abs(desloc - candi(:,2))./tRng;
                 pscore = candi(:,4)./xRng; 
                 score = pscore .* exp(-4*tscore);
