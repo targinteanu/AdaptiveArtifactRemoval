ec185df3498ec1aacb8532c6d7097d3a77203285
Toren Arginteanu <targinteanu@outlook.com>
2024-02-13 13:19:51 -0500

    new peaks extracted

--- batchExtractEERP.m
+++ batchExtractEERP.m
@@ -30,7 +30,7 @@ N = size(sig.Data_Unfiltered,1) - size(sig.Data_LMS_LPF,1) + 1;
 
 tBeforeTrig = .06;
 [t_PrePost, d_PrePost, e_PrePost] = getPrePostStim(tBeforeTrig, ...
-    sig.Noise_Reference, sig.Data_Unfiltered, sig.Data_LMS_LPF, sig.SampleRate, sig.Channels, N);
+    sig.Noise_Reference, sig.Data_HPF, sig.Data_LMS_LPF, sig.SampleRate, sig.Channels, N);
 %PrePostAvgAll_v2(tBeforeTrig,t_PrePost,d_PrePost,e_PrePost,Fs,sig.Channels,10);
 
 %% SNR (signal : noise ratio) - measure ERP peaks 
@@ -48,9 +48,9 @@ for ch = 1:size(ERPvals,2)
     nTrl = size(d_PrePost_ch,1);
 
     % setup tables for calculated values 
-    tblFilt = table('Size',[nTrl,6],...
-                    'VariableTypes',["double","double","double","double","double","double"], ...
-                    'VariableNames',{'n20amp','n20lat','n40amp','n40lat', 'mean','SD'});
+    tblFilt = table('Size',[nTrl,8],...
+                    'VariableTypes',["double","double","double","double","double","double","double","double"], ...
+                    'VariableNames',{'n07amp','n07lat','n15amp','n15lat','n40amp','n40lat', 'mean','SD'});
     tblUnfilt = tblFilt;
 
     % calculate values for each trial 
@@ -59,9 +59,9 @@ for ch = 1:size(ERPvals,2)
         %figure; subplot(211); 
 
         % populate row trl of table with n20 & n40 amps and latencies and stats (mean & SD)
-        [n20n40,~,stat] = measureERP(tPost, d_PrePost_ch(trl,:,2), [.01 .02 .04], [], [.008,.1], 200, false);
-        tblUnfilt.n10amp(trl) = n20n40(1,1); tblUnfilt.n10lat(trl) = n20n40(2,1); % [amplitude; latency]
-        tblUnfilt.n20amp(trl) = n20n40(1,2); tblUnfilt.n20lat(trl) = n20n40(2,2); % [amplitude; latency]
+        [n20n40,~,stat] = measureERP(tPost, d_PrePost_ch(trl,:,2), [.007 .015 .04], [], [.004,.1], 100, false);
+        tblUnfilt.n07amp(trl) = n20n40(1,1); tblUnfilt.n07lat(trl) = n20n40(2,1); % [amplitude; latency]
+        tblUnfilt.n15amp(trl) = n20n40(1,2); tblUnfilt.n15lat(trl) = n20n40(2,2); % [amplitude; latency]
         tblUnfilt.n40amp(trl) = n20n40(1,3); tblUnfilt.n40lat(trl) = n20n40(2,3); % [amplitude; latency]
         tblUnfilt.mean(trl) = stat(1); tblUnfilt.SD(trl) = std(d_PrePost_ch(trl,:,2)); 
             % mean = only within selected time range
@@ -72,9 +72,9 @@ for ch = 1:size(ERPvals,2)
         %subplot(212); 
 
         % do the same for filtered data 
-        [n20n40,~,stat] = measureERP(tPost, e_PrePost_ch(trl,:,2), [.01 .02 .04], [], [.008,.1], 200, false);
-        tblFilt.n10amp(trl) = n20n40(1,1); tblFilt.n10lat(trl) = n20n40(2,1); % [amplitude; latency]
-        tblFilt.n20amp(trl) = n20n40(1,2); tblFilt.n20lat(trl) = n20n40(2,2); % [amplitude; latency]
+        [n20n40,~,stat] = measureERP(tPost, e_PrePost_ch(trl,:,2), [.007 .015 .04], [], [.004,.1], 100, false);
+        tblFilt.n07amp(trl) = n20n40(1,1); tblFilt.n07lat(trl) = n20n40(2,1); % [amplitude; latency]
+        tblFilt.n15amp(trl) = n20n40(1,2); tblFilt.n15lat(trl) = n20n40(2,2); % [amplitude; latency]
         tblFilt.n40amp(trl) = n20n40(1,3); tblFilt.n40lat(trl) = n20n40(2,3); % [amplitude; latency]
         tblFilt.mean(trl) = stat(1); tblFilt.SD(trl) = std(e_PrePost_ch(trl,:,2)); 
         clear n20n40 stat
@@ -95,49 +95,49 @@ for ch = 1:size(ERPvals,2)
     bigTblUnfilt = ERPtables{1,ch}; bigTblFilt = ERPtables{2,ch};
 
 
-    bigTblUnfilt.n10ampMean(subjname) = mean(tblUnfilt.n10amp - tblUnfilt.mean, 'omitnan');
-    bigTblUnfilt.n20ampMean(subjname) = mean(tblUnfilt.n20amp - tblUnfilt.mean, 'omitnan');
+    bigTblUnfilt.n07ampMean(subjname) = mean(tblUnfilt.n07amp - tblUnfilt.mean, 'omitnan');
+    bigTblUnfilt.n15ampMean(subjname) = mean(tblUnfilt.n15amp - tblUnfilt.mean, 'omitnan');
     bigTblUnfilt.n40ampMean(subjname) = mean(tblUnfilt.n40amp - tblUnfilt.mean, 'omitnan');
-    bigTblUnfilt.n10latMean(subjname) = mean(tblUnfilt.n10lat, 'omitnan');
-    bigTblUnfilt.n20latMean(subjname) = mean(tblUnfilt.n20lat, 'omitnan');
+    bigTblUnfilt.n07latMean(subjname) = mean(tblUnfilt.n07lat, 'omitnan');
+    bigTblUnfilt.n15latMean(subjname) = mean(tblUnfilt.n15lat, 'omitnan');
     bigTblUnfilt.n40latMean(subjname) = mean(tblUnfilt.n40lat, 'omitnan');
-    bigTblUnfilt.snrMean(subjname) = mean( (tblUnfilt.n20amp-tblUnfilt.mean)./tblUnfilt.SD, 'omitnan' );
+    bigTblUnfilt.snrMean(subjname) = mean( (tblUnfilt.n15amp-tblUnfilt.mean)./tblUnfilt.SD, 'omitnan' );
     bigTblUnfilt.noiseMean(subjname) = mean(tblUnfilt.SD); 
     
-    bigTblUnfilt.n10ampSD(subjname) = std(tblUnfilt.n10amp - tblUnfilt.mean, 'omitnan');
-    bigTblUnfilt.n20ampSD(subjname) = std(tblUnfilt.n20amp - tblUnfilt.mean, 'omitnan');
+    bigTblUnfilt.n07ampSD(subjname) = std(tblUnfilt.n07amp - tblUnfilt.mean, 'omitnan');
+    bigTblUnfilt.n15ampSD(subjname) = std(tblUnfilt.n15amp - tblUnfilt.mean, 'omitnan');
     bigTblUnfilt.n40ampSD(subjname) = std(tblUnfilt.n40amp - tblUnfilt.mean, 'omitnan');
-    bigTblUnfilt.n10latSD(subjname) = std(tblUnfilt.n10lat, 'omitnan');
-    bigTblUnfilt.n20latSD(subjname) = std(tblUnfilt.n20lat, 'omitnan');
+    bigTblUnfilt.n07latSD(subjname) = std(tblUnfilt.n07lat, 'omitnan');
+    bigTblUnfilt.n15latSD(subjname) = std(tblUnfilt.n15lat, 'omitnan');
     bigTblUnfilt.n40latSD(subjname) = std(tblUnfilt.n40lat, 'omitnan');
-    bigTblUnfilt.snrSD(subjname) = std( (tblUnfilt.n20amp-tblUnfilt.mean)./tblUnfilt.SD, 'omitnan' );
+    bigTblUnfilt.snrSD(subjname) = std( (tblUnfilt.n15amp-tblUnfilt.mean)./tblUnfilt.SD, 'omitnan' );
     bigTblUnfilt.noiseSD(subjname) = std(tblUnfilt.SD); 
 
-    bigTblUnfilt.n10num(subjname) = sum(~isnan(tblUnfilt.n10lat));
-    bigTblUnfilt.n20num(subjname) = sum(~isnan(tblUnfilt.n20lat));
+    bigTblUnfilt.n07num(subjname) = sum(~isnan(tblUnfilt.n07lat));
+    bigTblUnfilt.n15num(subjname) = sum(~isnan(tblUnfilt.n15lat));
     bigTblUnfilt.n40num(subjname) = sum(~isnan(tblUnfilt.n40lat));
 
 
-    bigTblFilt.n10ampMean(subjname) = mean(tblFilt.n10amp - tblFilt.mean, 'omitnan');
-    bigTblFilt.n20ampMean(subjname) = mean(tblFilt.n20amp - tblFilt.mean, 'omitnan');
+    bigTblFilt.n07ampMean(subjname) = mean(tblFilt.n07amp - tblFilt.mean, 'omitnan');
+    bigTblFilt.n15ampMean(subjname) = mean(tblFilt.n15amp - tblFilt.mean, 'omitnan');
     bigTblFilt.n40ampMean(subjname) = mean(tblFilt.n40amp - tblFilt.mean, 'omitnan');
-    bigTblFilt.n10latMean(subjname) = mean(tblFilt.n10lat, 'omitnan');
-    bigTblFilt.n20latMean(subjname) = mean(tblFilt.n20lat, 'omitnan');
+    bigTblFilt.n07latMean(subjname) = mean(tblFilt.n07lat, 'omitnan');
+    bigTblFilt.n15latMean(subjname) = mean(tblFilt.n15lat, 'omitnan');
     bigTblFilt.n40latMean(subjname) = mean(tblFilt.n40lat, 'omitnan');
-    bigTblFilt.snrMean(subjname) = mean( (tblFilt.n20amp-tblFilt.mean)./tblFilt.SD, 'omitnan' );
+    bigTblFilt.snrMean(subjname) = mean( (tblFilt.n15amp-tblFilt.mean)./tblFilt.SD, 'omitnan' );
     bigTblFilt.noiseMean(subjname) = mean(tblFilt.SD); 
     
-    bigTblFilt.n10ampSD(subjname) = std(tblFilt.n10amp - tblFilt.mean, 'omitnan');
-    bigTblFilt.n20ampSD(subjname) = std(tblFilt.n20amp - tblFilt.mean, 'omitnan');
+    bigTblFilt.n07ampSD(subjname) = std(tblFilt.n07amp - tblFilt.mean, 'omitnan');
+    bigTblFilt.n15ampSD(subjname) = std(tblFilt.n15amp - tblFilt.mean, 'omitnan');
     bigTblFilt.n40ampSD(subjname) = std(tblFilt.n40amp - tblFilt.mean, 'omitnan');
-    bigTblFilt.n10latSD(subjname) = std(tblFilt.n10lat, 'omitnan');
-    bigTblFilt.n20latSD(subjname) = std(tblFilt.n20lat, 'omitnan');
+    bigTblFilt.n07latSD(subjname) = std(tblFilt.n07lat, 'omitnan');
+    bigTblFilt.n15latSD(subjname) = std(tblFilt.n15lat, 'omitnan');
     bigTblFilt.n40latSD(subjname) = std(tblFilt.n40lat, 'omitnan');
-    bigTblFilt.snrSD(subjname) = std( (tblFilt.n20amp-tblFilt.mean)./tblFilt.SD, 'omitnan' );
+    bigTblFilt.snrSD(subjname) = std( (tblFilt.n15amp-tblFilt.mean)./tblFilt.SD, 'omitnan' );
     bigTblFilt.noiseSD(subjname) = std(tblFilt.SD); 
 
-    bigTblFilt.n10num(subjname) = sum(~isnan(tblFilt.n10lat));
-    bigTblFilt.n20num(subjname) = sum(~isnan(tblFilt.n20lat));
+    bigTblFilt.n07num(subjname) = sum(~isnan(tblFilt.n07lat));
+    bigTblFilt.n15num(subjname) = sum(~isnan(tblFilt.n15lat));
     bigTblFilt.n40num(subjname) = sum(~isnan(tblFilt.n40lat));
 
     ERPtables{1,ch} = bigTblUnfilt; ERPtables{2,ch} = bigTblFilt;
