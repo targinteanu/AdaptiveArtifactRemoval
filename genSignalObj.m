087e98c0e04bb791d74b8d79dd06d694a8c644b9
Toren Arginteanu <targinteanu@outlook.com>
2024-02-29 16:59:30 -0500

    catch errors and move on to next file

--- genSignalObj.m
+++ genSignalObj.m
@@ -16,20 +16,24 @@ savedir = [folder,filesep,'saved_signal_obj',filesep];
 
 if contains(folder,'day') % for Kiara files
     for idx = 3:size(files,1)
-%        cd(folder); %brings loop back to the outer folder
+        %        cd(folder); %brings loop back to the outer folder
         fname = [folder,filesep,files(idx,1).name];
-    if isfolder(fname)
-        [sigN, sigV, trlName] = RodentNerveToSigWrapper(fname);
-        for trl = 1:length(trlName)
-            sigNt = sigN(trl); sigVt = sigV(trl);
-            if ~isempty(sigNt.Times)
-                saveSignalObj([savedir,files(idx,1).name,'_',trlName{trl},'_Naive','_saved'],sigNt);
-            end
-            if ~isempty(sigVt.Times)
-                saveSignalObj([savedir,files(idx,1).name,'_',trlName{trl},'_VDMT','_saved'],sigVt);
+        if isfolder(fname)
+            try
+                [sigN, sigV, trlName] = RodentNerveToSigWrapper(fname);
+                for trl = 1:length(trlName)
+                    sigNt = sigN(trl); sigVt = sigV(trl);
+                    if ~isempty(sigNt.Times)
+                        saveSignalObj([savedir,files(idx,1).name,'_',trlName{trl},'_Naive','_saved'],sigNt);
+                    end
+                    if ~isempty(sigVt.Times)
+                        saveSignalObj([savedir,files(idx,1).name,'_',trlName{trl},'_VDMT','_saved'],sigVt);
+                    end
+                end
+            catch ME
+                warning([fname,' not saved: ',ME.message]);
             end
         end
-    end
     end
 cd(codedir);
 
